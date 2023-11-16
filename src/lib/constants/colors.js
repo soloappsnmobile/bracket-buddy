494bf0871fbd3c42d2a44e9154d03e83f74bf615
soloappsnmobile <solomon@appsnmobilesolutions.com>
2023-11-16 10:51:50 +0000

    initial commit

---
+++ src/lib/constants/colors.js
@@ -0,0 +1,16 @@
+const colors = {
+  white: "#ffffff",
+  whiteAlt: "#f8f9fa",
+  whiteLight: "rgba(255, 255, 255, 0.9)",
+  black: "#000000",
+  blackLight: "rgba(0, 0, 0, 0.5)",
+  gray: "#808080",
+  darkGray: "#404040",
+  lightGray: "#d3d3d3",
+  success: "#00b300",
+  successLighter: "rgba(0, 179, 0, 0.1)",
+  error: "#ff0000",
+  primary: "#35B32A",
+};
+
+export default colors;
---
+++ src/lib/constants/index.js
@@ -0,0 +1,5 @@
+import colors from "./colors";
+import dimensions from "./dimensions";
+import dummy from "./dummy";
+
+export { colors, dimensions, dummy };
